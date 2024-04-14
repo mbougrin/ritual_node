de78fcc0beb2ef6908d3e0319e447414a36afe1b
mbougrin <mbougrin@student.42.fr>
2024-04-14 17:42:20 +0200

    adding ritual script install

---
+++ script_ritual.sh
@@ -0,0 +1,30 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt -qy install curl git jq lz4 build-essential screen
+
+sudo apt install docker.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+git clone https://github.com/ritual-net/infernet-container-starter
+
+cd infernet-container-starter
+
+screen -S ritual
+
+sudo project=hello-world make deploy-container -d
+
+# ctrl A + D for qui
+# return on it (screen -ls) - (screen -x %d return on screen ls)
+
+vi deploy/config.json
+
+#add api key https alchemy (Base/Base Mainnet)
+
+sudo docker restart anvil-node
+sudo docker restart hello-world
+sudo docker restart deploy-node-1
+sudo docker restart deploy-fluentbit-1
+sudo docker restart deploy-redis-1
